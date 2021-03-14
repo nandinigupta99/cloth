c426a51e17ba7cb7cd1238516fb34a6110273fb3
Nandini <nandinig819@gmail.com>
2021-03-14 20:40:51 +0530

    form input

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
+import SignInSignOut from "./pages/signin-and-signout/signin-and-signout.component";
 
+const HatsPage = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+  );
+};
 class App extends Component {
   render() {
-    return <div className='App'>Hello World</div>;
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/hats" component={HatsPage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInSignOut} />
+        </Switch>
+      </div>
+    );
   }
 }
 
---
+++ src/pages/signin-and-signout/signin-and-signout.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./signin-signout.css";
+import SignIn from "../../components/sign-in/sign-in.component";
+
+const SignInSignOut = () => (
+  <div className="sign-in-and-sign-up">
+    <SignIn />
+  </div>
+);
+export default SignInSignOut;
